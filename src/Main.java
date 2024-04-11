26690d0d5d06a55de986af3be8d02f0c8e2c7832
ferd1az <fdiaz@elpuig.xeill.net>
2024-04-11 19:41:30 +0200

    Primeros canvios

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Fer, David, Jaider");
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+
+    private static void method1() {
+    }
+
+    private static void method2() {
+    }
+    private static void method3() {
+    }
+    private static void method4() {
     }
 }
